26467d2ab8528d10b57692538d58eadebf341ae6
Josh <zylo.codes@gmail.com>
2024-06-26 21:19:28 -0500

    DOM module

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -23,3 +23,70 @@ export const createAlert = ({ text, color = 'alert-info' }: CreateAlertArgs) => 
   alert.innerText = text
   return alert
 }
+
+interface CreateButtonArgs {
+  text: string
+  color?: ButtonColor
+  type?: 'button' | 'submit'
+  onClick?(event: Event): void
+}
+
+export const createButton = ({
+  text,
+  color = 'btn-info',
+  type = 'button',
+  onClick,
+}: CreateButtonArgs) => {
+  const button = document.createElement('button')
+  button.classList.add('btn')
+  button.classList.add(color)
+  button.innerText = text
+  button.type = type
+  if (onClick) {
+    button.addEventListener('click', onClick)
+  }
+  return button
+}
+
+
+interface CreateInputArgs {
+  name: string
+  required?: boolean
+  type: HTMLInputElement['type']
+}
+
+export const createInput = ({
+  name,
+  required = false,
+  type = 'text'
+}: CreateInputArgs) => {
+  const input = document.createElement('input')
+  input.name = name
+  input.type = type
+  input.classList.add('form-control')
+  input.required = required
+  return input
+}
+
+export const renderFinalScoreAlert = (score: number, numQuestions: number) => {
+  if (!quizContainerEl) throw Error('Missing quizContainerEl in DOM')
+  quizContainerEl.innerHTML = ''
+  
+  const scorePercentage = (score / numQuestions) * 100
+  let color: AlertColor = 'alert-danger'
+  let text: string = 'Better luck next time.'
+
+  if (scorePercentage >= 80) {
+    color = 'alert-success'
+    text = 'Great job!'
+  } else if (scorePercentage < 80 && scorePercentage > 67) {
+    color = 'alert-warning'
+    text = 'Not too shabby'
+  }
+
+  text = `Score ${score}/${numQuestions} (${scorePercentage}%). ${text}`
+
+  const alert = createAlert({ text, color })
+
+  return alert
+}
